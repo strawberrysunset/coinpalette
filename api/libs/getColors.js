4986e83b3a674e779676d035e013f440ce956c66
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-01 02:28:04 +0000

    Return object instead of array.

--- api/libs/getColors.js
+++ api/libs/getColors.js
@@ -1,11 +1,18 @@
 const colorList = require('../colorList.json');
 
-// Get a list of colors for specified colors and color format.
-module.exports = function getColors({ coins, format }) {
+// Get a list of colors for specified coins.
+module.exports = function getColors({ coins }) {
 
     // Return all if none specified.
     if(!coins) return colorList 
 
+    let selected = {}
+    for (coin of coins) {
+      selected[coin] = colorList[coin]
+    }
+
     // Filter based on specified coins.
-    return coins.map(coin => ({[coin] : colorList[coin]}));
+    return selected
 }
+
+
